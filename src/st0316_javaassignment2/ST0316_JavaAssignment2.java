0178532d0c46c76616d4f1908d1fd5d8963f412b
MianBaoChaoRen <mianbaocr@gmail.com>
2016-07-06 10:23:51 +0800

    First Time

---
+++ src/st0316_javaassignment2/ST0316_JavaAssignment2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package st0316_javaassignment2;
+
+/**
+ *
+ * @author Marcus Tan
+ *         Theodoric Keith Lim
+ */
+public class ST0316_JavaAssignment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("TESTING");
+    }
+    
+}
